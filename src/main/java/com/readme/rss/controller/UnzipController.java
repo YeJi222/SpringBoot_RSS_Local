68dba3144fb0dc14fc47da23d2c969a718fed231
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-03-12 04:34:13 +0900

    feat: add deleteUnzipFiles function

--- src/main/java/com/readme/rss/controller/UnzipController.java
+++ src/main/java/com/readme/rss/controller/UnzipController.java
@@ -8,7 +8,6 @@ import java.net.URI;
 import java.util.Scanner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -82,9 +81,37 @@ public class UnzipController {
         }
     }
 
+    private static void deleteUnzipFiles(ProcessBuilder builder) throws IOException {
+        builder.command("rm", "-rf", "./unzipFiles/");
+        var process = builder.start(); // upzip한 파일들 모두 삭제
+
+        try (var reader = new BufferedReader(
+            new InputStreamReader(process.getInputStream()))) {
+            String commandResult;
+            while ((commandResult = reader.readLine()) != null) {
+                System.out.println(commandResult);
+            }
+        }
+        System.out.println("압축풀기한 파일들 모두 삭제 완료");
+    }
+
     public static void main(String[] args) throws IOException {
-        // 파일 압축 풀기
+        // 쉘 명령어 실행을 위한 ProcessBuilder 사용
         ProcessBuilder builder = new ProcessBuilder();
+
+        // unzipFiles 폴더 생성 - 압축풀기한 파일들을 저장하는 임시 폴더
+        builder.command("mkdir", "unzipFiles");
+        builder.start();
+
+        File dirFile = new File("./unzipFiles");
+        File[] fileList = dirFile.listFiles();
+
+        if(fileList.length != 0){ // 기존에 압축풀기한 파일들이 존재하면 기존 파일들 삭제하고 시작
+            System.out.println("기존 파일들 존재! 삭제하고 시작!");
+            deleteUnzipFiles(builder);
+        }
+
+        // 파일 압축 풀기
         builder.command("unzip", "unzipTest.zip", "-d", "./unzipFiles");
         var process = builder.start(); // upzip 실행
 
@@ -99,8 +126,11 @@ public class UnzipController {
 
         // 압축 푼 파일들 중에서 원하는 정보 찾기(ex. url 찾기)
         String searchDirPath = "./unzipFiles";
-        System.out.println("[압축 해제한 폴더 속 파일 리스트]");
+        System.out.println("\n[압축 해제한 폴더 속 파일 리스트]");
         searchFiles(searchDirPath);
+
+        // content data 보냈으므로, 압축풀기한 파일들 모두 삭제
+        deleteUnzipFiles(builder);
     }
 
     final String getMappingValue = "/url";
